9639dd773ca4ceff2d72a40568e43c92047a3388
Anish Shankar <rndanish@gmail.com>
2013-01-12 14:59:06 +0530

    Added chunk size

--- src/com/phinvader/libjdcpp/DCConstants.java
+++ src/com/phinvader/libjdcpp/DCConstants.java
@@ -2,9 +2,16 @@ package com.phinvader.libjdcpp;
 
 /**
  * This is a class just to store all related constants
+ * 
  * @author phinfinity
  * 
  */
 public class DCConstants {
 	public static String version = "libjdcpp v1.0";
+
+	/**
+	 * This is the number of bytes per chunk sent while transfering files over
+	 * the TCP connection
+	 */
+	public static long data_chunk_size = 40906;
 }
