8ca57a3d20fc24889abbd95a52e856216f01ca04
Anish Shankar <rndanish@gmail.com>
2013-01-12 14:12:30 +0530

    Added a structure for storing DC preferences and settings

---
+++ src/com/phinvader/libjdcpp/DCPreferences.java
@@ -0,0 +1,29 @@
+package com.phinvader.libjdcpp;
+
+public class DCPreferences {
+
+	public String nick;
+	public long share_size; 
+
+	/**
+	 * Save preferences to an encoded format for storage.
+	 * 
+	 * @return
+	 * @see DCPreferences#load_preferences(String)
+	 */
+	public String save_preferences() {
+		// TODO auto-generated block
+		return null;
+	}
+
+	/**
+	 * Load preferences from a single string encoded format
+	 * 
+	 * @param pref_string
+	 * @see DCPreferences#save_preferences()
+	 */
+	public void load_preferences(String pref_string) {
+		// TODO auto-generated block
+	}
+
+}
