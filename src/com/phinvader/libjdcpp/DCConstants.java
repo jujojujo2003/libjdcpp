77937739dd336727c4eff780310cc849864923cb
Anish Shankar <rndanish@gmail.com>
2013-01-12 14:44:07 +0530

    Added constants file

---
+++ src/com/phinvader/libjdcpp/DCConstants.java
@@ -0,0 +1,10 @@
+package com.phinvader.libjdcpp;
+
+/**
+ * This is a class just to store all related constants
+ * @author phinfinity
+ * 
+ */
+public class DCConstants {
+	public static String version = "libjdcpp v1.0";
+}
