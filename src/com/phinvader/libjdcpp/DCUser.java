c80635bd6add60e089ab27ebc7f1af3af4981c5b
Anish Shankar <rndanish@gmail.com>
2013-01-14 14:43:27 +0530

    Added class to store DCUser details in a Hub's NickList

---
+++ src/com/phinvader/libjdcpp/DCUser.java
@@ -0,0 +1,20 @@
+package com.phinvader.libjdcpp;
+
+/**
+ * The only compulsory field here is nick. Rest all may be null.
+ * 
+ * @author phinfinity
+ *
+ */
+public class DCUser {
+	public String nick;
+	public String ip;
+	public long share_size;
+	public String description;
+	public String tag;
+	public String email;
+	public String connection;
+	public boolean active;
+	public boolean op;
+	
+}
