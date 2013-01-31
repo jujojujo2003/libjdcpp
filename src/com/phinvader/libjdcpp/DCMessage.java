fc320538f4e11d7b35a8ac6651188380ac9fe8f8
Anish Shankar <rndanish@gmail.com>
2013-02-01 01:33:38 +0530

    Added some parse guards to catch parse exceptions. and fixed one parse error

--- src/com/phinvader/libjdcpp/DCMessage.java
+++ src/com/phinvader/libjdcpp/DCMessage.java
@@ -62,113 +62,129 @@ public class DCMessage {
 	public void parse(byte[] input) {
 
 		boolean parse_success = true;
-		if (input[0] == '$') {
-			int beg = DCFunctions.find_next(input, 1, ' ');
-			if (beg == -1)
-				beg = input.length;
-			command = new String(input, 1, beg - 1);
-			if (command.equals("Lock")) {
-				int poffset = beg + 1, coffset;
-				coffset = DCFunctions.find_next(input, poffset, ' ');
-				lock_s = new String(input, poffset, coffset - poffset);
-				poffset = coffset + 1;
-				coffset = DCFunctions.find_next(input, poffset, ' ');
-				String[] key_split = new String(input, poffset, coffset
-						- poffset).split("=");
-				if (key_split.length != 2
-						|| !key_split[0].toLowerCase().equals("pk"))
-					parse_success = false;
-				else
-					key_s = key_split[1];
-
-			} else if (command.equals("Key")) {
-				key_s = new String(input, beg + 1, input.length - beg - 1);
-			} else if (command.equals("HubName")) {
-				hubname_s = new String(input, beg + 1, input.length - beg - 1);
-			} else if (command.equals("Supports")) {
-				String sup_s = new String(input, beg + 1, input.length - beg
-						- 1);
-				supports = sup_s.split(" ");
-			} else if (command.equals("Hello")) {
-				hello_s = new String(input, beg + 1, input.length - beg - 1);
-			} else if (command.equals("MyINFO")) {
-				myinfo = new DCUser();
-				DCFunctions.find_next(input, beg, '$');
-				int offsets[] = new int[6];
-				int seglength[] = new int[6];
-				int loffset = DCFunctions.find_next(input, beg + 1, '$');
-				// $ALL
-				offsets[0] = loffset;
-				loffset = DCFunctions.find_next(input, loffset, ' ');
-				seglength[0] = loffset - offsets[0];
-				loffset++;
-				// Nick Name
-				offsets[1] = loffset;
-				loffset = DCFunctions.find_next(input, loffset, ' ');
-				seglength[1] = loffset - offsets[1];
-				loffset++;
-				// Interest String
-				offsets[2] = loffset;
-				loffset = DCFunctions.find_next(input, loffset, '$');
-				seglength[2] = loffset - offsets[2];
-				loffset++;
-				// Speed specifier
-				loffset = DCFunctions.find_next(input, loffset, '$') + 1; // skipping
-																			// blankspace
-				offsets[3] = loffset;
-				loffset = DCFunctions.find_next(input, loffset, '$');
-				seglength[3] = loffset - offsets[3];
-				loffset++;
-				// email
-				offsets[4] = loffset;
-				loffset = DCFunctions.find_next(input, loffset, '$');
-				seglength[4] = loffset - offsets[4];
-				loffset++;
-				// Share Size
-				offsets[5] = loffset;
-				loffset = DCFunctions.find_next(input, loffset, '$');
-				seglength[5] = loffset - offsets[5];
-				if (new String(input, offsets[0], seglength[0]).equals("$ALL")) {
-					myinfo.nick = new String(input, offsets[1], seglength[1]);
-					myinfo.description = new String(input, offsets[2],
-							seglength[2]);
-					int desc_split_offset = myinfo.description.lastIndexOf('<');
-					// Split for the tag
-					myinfo.tag = myinfo.description
-							.substring(desc_split_offset);
-					if (myinfo.tag.contains("M:A"))
-						myinfo.active = true;
+		try {
+			if (input[0] == '$') {
+				int beg = DCFunctions.find_next(input, 1, ' ');
+				if (beg == -1)
+					beg = input.length;
+				command = new String(input, 1, beg - 1);
+				if (command.equals("Lock")) {
+					int poffset = beg + 1, coffset;
+					coffset = DCFunctions.find_next(input, poffset, ' ');
+					lock_s = new String(input, poffset, coffset - poffset);
+					poffset = coffset + 1;
+					coffset = DCFunctions.find_next(input, poffset, ' ');
+					String[] key_split = new String(input, poffset, coffset
+							- poffset).split("=");
+					if (key_split.length != 2
+							|| !key_split[0].toLowerCase().equals("pk"))
+						parse_success = false;
 					else
-						myinfo.active = false;
-					myinfo.description = myinfo.description.substring(0,
-							desc_split_offset);
-					myinfo.connection_speed = new String(input, offsets[3],
-							seglength[3] - 1);
-					myinfo.speed_id = input[offsets[3] + seglength[3] - 1];
-					myinfo.email = new String(input, offsets[4], seglength[4]);
-					myinfo.share_size = Long.parseLong(new String(input,
-							offsets[5], seglength[5]));
+						key_s = key_split[1];
+
+				} else if (command.equals("Key")) {
+					key_s = new String(input, beg + 1, input.length - beg - 1);
+				} else if (command.equals("HubName")) {
+					hubname_s = new String(input, beg + 1, input.length - beg
+							- 1);
+				} else if (command.equals("Supports")) {
+					String sup_s = new String(input, beg + 1, input.length
+							- beg - 1);
+					supports = sup_s.split(" ");
+				} else if (command.equals("Hello")) {
+					hello_s = new String(input, beg + 1, input.length - beg - 1);
+				} else if (command.equals("MyINFO")) {
+					myinfo = new DCUser();
+					DCFunctions.find_next(input, beg, '$');
+					int offsets[] = new int[6];
+					int seglength[] = new int[6];
+					int loffset = DCFunctions.find_next(input, beg + 1, '$');
+					// $ALL
+					offsets[0] = loffset;
+					loffset = DCFunctions.find_next(input, loffset, ' ');
+					seglength[0] = loffset - offsets[0];
+					loffset++;
+					// Nick Name
+					offsets[1] = loffset;
+					loffset = DCFunctions.find_next(input, loffset, ' ');
+					seglength[1] = loffset - offsets[1];
+					loffset++;
+					// Interest String
+					offsets[2] = loffset;
+					loffset = DCFunctions.find_next(input, loffset, '$');
+					seglength[2] = loffset - offsets[2];
+					loffset++;
+					// Speed specifier
+					loffset = DCFunctions.find_next(input, loffset, '$') + 1; // skipping
+																				// blankspace
+					offsets[3] = loffset;
+					loffset = DCFunctions.find_next(input, loffset, '$');
+					seglength[3] = loffset - offsets[3];
+					loffset++;
+					// email
+					offsets[4] = loffset;
+					loffset = DCFunctions.find_next(input, loffset, '$');
+					seglength[4] = loffset - offsets[4];
+					loffset++;
+					// Share Size
+					offsets[5] = loffset;
+					loffset = DCFunctions.find_next(input, loffset, '$');
+					seglength[5] = loffset - offsets[5];
+					if (new String(input, offsets[0], seglength[0])
+							.equals("$ALL")) {
+						myinfo.nick = new String(input, offsets[1],
+								seglength[1]);
+						myinfo.description = new String(input, offsets[2],
+								seglength[2]);
+						int desc_split_offset = myinfo.description
+								.lastIndexOf('<');
+						// Split for the tag
+						if (desc_split_offset != -1) {
+							myinfo.tag = myinfo.description
+									.substring(desc_split_offset);
+							myinfo.description = myinfo.description.substring(
+									0, desc_split_offset);
+						} else {
+							myinfo.tag = "";
+						}
+						if (myinfo.tag.contains("M:A"))
+							myinfo.active = true;
+						else
+							myinfo.active = false;
+						myinfo.connection_speed = new String(input, offsets[3],
+								seglength[3] - 1);
+						myinfo.speed_id = input[offsets[3] + seglength[3] - 1];
+						myinfo.email = new String(input, offsets[4],
+								seglength[4]);
+						myinfo.share_size = Long.parseLong(new String(input,
+								offsets[5], seglength[5]));
+					} else {
+						parse_success = false;
+					}
+				} else if (command.equals("ValidateNick")) {
+				} else if (command.equals("Version")) {
+				} else if (command.equals("GetNickList")) {
+				} else if (command.equals("ConnectToMe")) {
+				} else if (command.equals("HubQuit")) {
+					quit_s = new String(input, beg + 1, input.length - beg - 1);
+					// return just command as HubQuit
+				} else if (command.equals("Quit")) {
+					quit_s = new String(input, beg + 1, input.length - beg - 1);
 				} else {
 					parse_success = false;
 				}
-			} else if (command.equals("ValidateNick")) {
-			} else if (command.equals("Version")) {
-			} else if (command.equals("GetNickList")) {
-			} else if (command.equals("ConnectToMe")) {
-			} else if (command.equals("HubQuit")) {
-				quit_s = new String(input, beg + 1, input.length - beg - 1);
-				// return just command as HubQuit
-			} else if (command.equals("Quit")) {
-				quit_s = new String(input, beg + 1, input.length - beg - 1);
 			} else {
 				parse_success = false;
 			}
-		} else {
-			parse_success = false;
-		}
-		if (!parse_success) {
+			if (!parse_success) {
+				command = null;
+				msg_s = new String(input);
+			}
+		} catch (Exception e) {
+			// TODO Remove This debugging piece
 			command = null;
-			msg_s = new String(input);
+			msg_s = "ParseFail : " + e.toString();
+			msg_s += "\n For:" + new String(input);
 		}
 	}
 
